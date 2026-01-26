14d9790b85a203f27e9f557ca8d2b1d2ba55ea57
noman1221 <naumaank0786@gmail.com>
2026-01-26 22:56:47 +0530

    solved binary search

--- basicDSA.Series.js
+++ basicDSA.Series.js
@@ -301,8 +301,26 @@
 // console.log(twoSum(arr));
 
 
+// 23. BINARY SEARCH,Algorithm: Binary Search (Divide and Conquer)
+let arr = [1, 2, 3, 4, 6];
 
+function binarySearch(arr, target) {
+    let i = 0;
+    let j = arr.length - 1;
 
+    while (i <= j) {
+        let mid = Math.floor((i + j) / 2);
 
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            i = mid + 1;
+        } else {
+            j = mid - 1;
+        }
+    }
 
+    return -1;
+}
+console.log(binarySearch(arr, 2));
 
