46d449d96837b449fbe8ffa6c0f7ea4b1155edb7
noman1221 <naumaank0786@gmail.com>
2026-02-01 23:29:04 +0530

    today i solved Bubble Sort

--- basicDSA.Series.js
+++ basicDSA.Series.js
@@ -570,3 +570,19 @@
 // console.log(unionArr);
 
 
+//36 Algorithm: Bubble Sort
+// let arr = [5, 3, 4, 1, 2];
+// function bubleSort(arr){
+//     for(let i  = 0; i<arr.length; i++){
+// for(let  j = i+1; j<arr.length; j++){
+// if (arr[i] >= arr[j]) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+// };
+// }
+// };
+// return arr;
+// }
+// console.log(bubleSort(arr));
+
